e9961263b81d27c64303bc795542a738e6d11df1
Adam Jeffries <adamljeffries@gmail.com>
2024-06-29 12:00:37 -0400

    added SQL Query

---
+++ SqlQuery_1.sql
@@ -0,0 +1,11 @@
+﻿SELECT Orders.OrderID, Orders.CustomerID, Customers.CustomerID, Customers.CustomerName
+FROM Orders 
+INNER JOIN Customers 
+ON Orders.CustomerID = Customers.CustomerID
+WHERE Orders.OrderID = 10310;
+
+SELECT Products.ProductID, Products.SupplierID, Suppliers.SupplierID, Suppliers.Address 
+FROM Products
+INNER JOIN Suppliers
+ON Products.SupplierID = Suppliers.SupplierID
+WHERE Products.ProductID = 40;
